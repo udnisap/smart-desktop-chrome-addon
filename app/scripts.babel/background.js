889e85514281e32e725f16a9fef68057028e8a84
sajini de silva <sdesilva28@gmail.com>
2016-05-29 06:03:11 +0530

    moved js files to different location

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,7 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
+chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-
+chrome.browserAction.setBadgeText({ text: '\'Allo' });
